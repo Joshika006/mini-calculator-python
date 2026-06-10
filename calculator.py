789ae8358dda651bf8671e94157059379f1bb203
Joshika006 <amalajoshika06@gmail.com>
2026-06-10 20:34:49 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+A=int(input("A:",))
+B=int(input("B:",))
+Operation=input("add/sub/div/mul:")
+if(Operation=="add"):
+    print(A+B)
+elif(Operation=="sub"):
+    print(A-B)
+elif(Operation=="div"):
+    print(A/B)
+elif(Operation=="mul"):
+    print(A*B)
+else:
+    print("Invalid operation")
+
+
+
